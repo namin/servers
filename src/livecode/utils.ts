9f37d6798b2ca0d6aeb6199e5fbf4bc422eae589
Nada Amin <namin@seas.harvard.edu>
2025-01-23 21:03:16 -0500

    bug fix

--- src/livecode/utils.ts
+++ src/livecode/utils.ts
@@ -3,8 +3,7 @@ import fs from 'fs';
 import path from 'path';
 
 export function snippetCache(content?: string): string {
-  if (!content) return '';
-  
+  content = content || '';
   const hash = crypto.createHash('sha256').update(content).digest('hex');
   const snippetPath = path.join('/tmp/snippets', hash);
   
